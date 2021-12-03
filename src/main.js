ffa1f5c2db988ba7118833b972e98012992d7301
ngthh <saccarozo.29@gmail.com>
2021-12-03 23:46:50 +0700

    Add some libraries

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
 import router from "./router";
 import store from "./store";
-import Vue2Editor from "vue2-editor";
 
-Vue.use(Vue2Editor);
+import Buefy from 'buefy'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
-Vue.config.productionTip = false;
+import {dom} from '@fortawesome/fontawesome-svg-core'
+import {fab} from '@fortawesome/free-brands-svg-icons'
+import {far} from '@fortawesome/free-regular-svg-icons'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {library} from '@fortawesome/fontawesome-svg-core'
+
+Vue.config.productionTip = false
+
+//FontAwesomeIcon
+dom.watch()
+library.add(fab, fas, far)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(Buefy, {
+  defaultIconComponent: 'font-awesome-icon',
+  defaultIconPack: 'fas'
+})
 
 new Vue({
+  render: h => h(App),
   router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+  store: store,
+}).$mount('#app')
