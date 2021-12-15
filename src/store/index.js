830f4c75908835ccd7f6ac05d61175f108fa5113
ngthh <saccarozo.29@gmail.com>
2021-12-16 00:00:37 +0700

    firebase

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+// import db from '../firebase/initFirebase'
+// import firebase from "firebase/app";
+import "firebase/auth";
 
 Vue.use(Vuex)
 
@@ -11,7 +14,12 @@ export default new Vuex.Store({
       { blogTitle: "Blog card 3", blogCoverPhoto: "stock-3", blogDate: "November 21, 2021"},
       { blogTitle: "Blog card 4", blogCoverPhoto: "stock-4", blogDate: "November 21, 2021"},
     ],
-    editPost: null
+    editPost: null,
+    user: null,
+    profileEmail: null,
+    profileName: null,
+    profileId: null,
+    profileInitials: null,
   },
   mutations: {
     tonggleEditPost(state, payload) {
